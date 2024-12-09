6b8ba53ce87f9a65a450f45355abe80372c85e49
mehedipro3 <mehediratul37@gmail.com>
2024-12-10 01:38:47 +0600

    server get done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
@@ -28,11 +28,18 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const usersCollection = client.db('quota').collection('data');
+    const usersCollection = client.db('quotaInjury').collection('datas');
 
     app.get("/users",async(req,res)=>{
       const cursor = usersCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/users/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await usersCollection.findOne(query);
       res.send(result);
     })
 
