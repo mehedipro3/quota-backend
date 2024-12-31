171a17909bfe8248f06e5526a1ed63bb0fa9a24a
mehedipro3 <mehediratul37@gmail.com>
2024-12-31 14:32:01 +0600

    first commit

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000 ;
-
+const port = process.env.PORT || 5000;
 
 // middleware 
-
 app.use(cors());
-app.use(express());
+app.use(express.json()); // Add this line to parse JSON bodies
 
+const uri = `mongodb+srv://new-users:YxKAFcmFHitG3izs@cluster0.ee44r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ee44r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,88 +21,100 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
     const dataCollection = client.db('quotaInjury').collection('datas');
-    // const usersCollection = client.db('quotaInjury').collection('users');
 
-    app.get("/datas",async(req,res)=>{
+    // GET all users
+    app.get("/datas", async (req, res) => {
       const cursor = dataCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.get("/datas/:id",async(req,res)=>{
+    // GET single user
+    app.get("/datas/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await dataCollection.findOne(query);
       res.send(result);
-    })
+    });
 
+    // CREATE new user
+    app.post("/datas", async (req, res) => {
+      const newUser = req.body;
+      const result = await dataCollection.insertOne(newUser);
+      // Return the created user with the generated _id
+      const createdUser = { ...newUser, _id: result.insertedId };
+      res.status(201).send(createdUser);
+      console.log('User created successfully', createdUser);
 
-    // to get a newUsers 
-    // app.post("/datas",async(req,res)=>{
-    //   const newUsers = req.body;
-    //   console.log(newUsers);
-    //   const result = await dataCollection.insertOne(newUsers);
-    //   res.send(result);
-    // })
+    });
 
-    //          /////// // USERS API // ////////
+    // UPDATE user
+    app.put("/datas/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: updatedUser.name,
+          medical_history: updatedUser.medical_history,
+          injury_date: updatedUser.injury_date,
+          age: updatedUser.age,
+          gender: updatedUser.gender,
+          contact_details: updatedUser.contact_details,
+          occupation: updatedUser.occupation,
+          blood_group: updatedUser.blood_group,
+          current_status: updatedUser.current_status,
+          treatment_type: updatedUser.treatment_type,
+          fund_amount_bdt: updatedUser.fund_amount_bdt,
+          fund_status: updatedUser.fund_status,
+          transaction_date: updatedUser.transaction_date,
+          receiver_amount_bdt: updatedUser.receiver_amount_bdt,
+          transaction_methods: updatedUser.transaction_methods,
+          transaction_id: updatedUser.transaction_id,
+          required_support: updatedUser.required_support,
+          incident_spot: updatedUser.incident_spot,
+          img: updatedUser.img
+        }
+      };
+      const result = await dataCollection.updateOne(filter, updateDoc);
+      if (result.matchedCount === 0) {
+        res.status(404).send({ message: "User not found" });
+        return;
+      }
+      // Return the updated user
+      const updated = await dataCollection.findOne(filter);
+      res.send(updated);
+    });
 
-    // app.get('/users', async (req, res) => {
-    //   const cursor = usersCollection.find();
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // })
-
-    // app.patch('/users',async(req,res)=>{
-    //   const email = req.body.email;
-    //   const filter = {email};
-    //   const updatedDoc = {
-    //     $set :{
-    //         lastSignInTime : req.body?.lastSignInTime
-    //     }
-    //   }
-    //   const result = await usersCollection.updateOne(filter,updatedDoc);
-    //   res.send(result);
-    // })
-
-    // app.post("/users", async (req, res) => {
-    //   const newUsers = req.body;
-    //   const result = await usersCollection.insertOne(newUsers);
-    //   res.send(result);
-    // })
-
-
-
-    // app.delete("/users/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await usersCollection.deleteOne(query);
-    //   res.send(result);
-    // })
-
-
-
-
+    // DELETE user
+    app.delete("/datas/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await dataCollection.deleteOne(query);
+      if (result.deletedCount === 0) {
+        res.status(404).send({ message: "User not found" });
+        return;
+      }
+      res.send({ message: "User deleted successfully" });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  } catch (error) {
+    console.error("Error:", error);
+    process.exit(1);
   }
 }
+
 run().catch(console.dir);
 
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send('Server is running');
 });
 
-app.listen(port,()=>{
-  console.log(`server is running on port : ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
